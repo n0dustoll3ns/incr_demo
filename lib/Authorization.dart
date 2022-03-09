86da0102054c529d5127ad4c24bf78ae35fb4995
n0dustoll3ns <fominykh.dg@gmail.com>
2022-03-09 12:38:31 +0700

    Add asset images to pubspec

--- lib/Authorization.dart
+++ lib/Authorization.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -15,9 +16,9 @@ class _LoginPageState extends State<LoginPage> {
             child: ListView(
       padding: const EdgeInsets.symmetric(horizontal: 24.0),
       children: <Widget>[
-        Image.asset('assets/diamond.png'),
+        Image.asset("assets/images/btc.png"),
         const SizedBox(height: 16.0),
-        const Text('SHRINE'),
+        const Text('TradeBits'),
       ],
     )));
   }
