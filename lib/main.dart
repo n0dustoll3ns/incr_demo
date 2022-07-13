b09a13e91b15bf22f601d9a8648351103668bd07
n0dustoll3ns <fominykh.dg@gmail.com>
2022-07-13 23:31:06 +0300

    add tooltip

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        tooltip: 'Summarize',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
